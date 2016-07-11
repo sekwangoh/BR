63cbd0b29a67e791b793e189b2449be1648bc151
sekwnag oh <osktr5@gmail.com>
2016-07-11 10:54:43 +0900

    댓글까지

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,21 +1,48 @@
 class PostsController < ApplicationController
 
 	def new
+		@post = Post.new
 	end
 
 	def create
 		@post = Post.new(post_params)
 
-		@post.save
-		redirect_to @post
+		if @post.save
+			redirect_to @post
+		else
+			render 'new'
+		end	
 	end
+
+	def edit
+		@post = Post.find(params[:id])
+	end	
+
+	def update
+		@post = Post.find(parmas[:id])
+
+		if @post.update(article_params)
+			redirect_to @post
+		else
+			render 'edit'
+		end
+	end
+
+	def destory
+		@post = Post.find(params[:id])
+		@post.destory
+
+		redirect_to post_path
+	end	 
+			
+	
 
 	def show
 		@post = Post.find(params[:id])
 	end	
 
 	def index
-		@post = Post.all
+		@posts = Post.all
 	end	
 		
 	def post_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
-   resources :posts
+   resources :posts 
+    resources :comments
 
    root 'posts#index'
   # The priority is based upon order of creation: first created -> highest priority.
